9c6d8aeb0000c1846b0187deb05066a417a4e402
xiangcitoZ <pzxiang1@gmail.com>
2023-03-29 13:22:36 +0200

    Add project files.

---
+++ LogicaPrimerServicioWCF/IMetodosVariosContract.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaPrimerServicioWCF
+{
+    [ServiceContract]
+    public interface IMetodosVariosContract
+    {
+        [OperationContract]
+        List<int> GetTablaMultiplicar(int numero);
+        [OperationContract]
+        int GetNumeroDoble(int numero);
+        [OperationContract]
+        string GetSaludo(string nombre);
+        string MetodoInvisible();
+
+    }
+}
---
+++ LogicaPrimerServicioWCF/MetodosVariosClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaPrimerServicioWCF
+{
+    internal class MetodosVariosClass : IMetodosVariosContract
+    {
+        public int GetNumeroDoble(int numero)
+        {
+            return numero * 2;
+        }
+
+        public string GetSaludo(string nombre)
+        {
+            return "Mi primer super Servicio!!!, " + nombre;
+
+        }
+
+        public List<int> GetTablaMultiplicar(int numero)
+        {
+            List<int> tabla = new List<int>();
+            for(int i = 1; i<= 10; i++) 
+            {
+                int resultado = numero * i;
+                tabla.Add(resultado);
+            }
+            return tabla;
+        }
+
+        public string MetodoInvisible()
+        {
+            return "No sirvo para nada, soy invisible";
+        }
+    }
+}
